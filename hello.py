d027328efad7eca4a5e2437319f53387de6c8934
Raju Kshetri <deecell.98@gmail.com>
2017-03-14 11:55:03 +0530

    Add initial code

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
-import os
+from flask import Flask
 
-print "hello world!"
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return "hello world!"
+
+if __name__ == '__main__':
+	app.run(port=5000,debug=True)
